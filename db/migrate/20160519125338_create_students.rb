09c613c4acb3beab00efe094fbb65eb6fad806d0
NamHyeongKeol <grey_pastor@naver.com>
2016-05-19 23:59:59 +0900

    rails new, univ, stud 테이블 만듦

---
+++ db/migrate/20160519125338_create_students.rb
@@ -0,0 +1,11 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.integer :university_id, null: false, default: 0
+      t.string :name, null: false, default: ""
+      t.string :student_number, null: false, default: ""
+
+      t.timestamps null: false
+    end
+  end
+end
