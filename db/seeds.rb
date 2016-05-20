013dfe6bc03e8944e8304bfa81e64f127c580142
NamHyeongKeol <grey_pastor@naver.com>
2016-05-20 19:49:43 +0900

    seed 만들기 시작

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+University.create(name: "cba")
+University.create(name: "eng")
+University.create(name: "art")
+University.create(name: "edu")
+University.create(name: "cls")
+University.create(name: "social")
+University.create(name: "cals")
+
+Professor.create(name: "ucheol2")
+Professor.create(name: "JacktheLee")
+Professor.create(name: "miniddong")
+Professor.create(name: "pjhjohn")
+Professor.create(name: "CallMeJustNam")
+
